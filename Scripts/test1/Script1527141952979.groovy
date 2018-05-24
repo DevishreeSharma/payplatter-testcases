6e3775cb469c62c8b0628b879987ff6c2df02be8
Dipali <dipali@dexpertsystems.com>
2018-05-24 12:25:49 +0530

    changes made

--- Scripts/test1/Script1527141952979.groovy
+++ Scripts/test1/Script1527141952979.groovy
@@ -1,21 +1,49 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
-import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('http://localhost:8085/BizPlatter/')
+
+WebUI.maximizeWindow()
+
+WebUI.delay(1)
+
+WebUI.click(findTestObject('test/Page_PayPlatter  Home  payplatter.i/a_Login'))
+
+String c = findTestData('login_users').getValue(14, 1)
+
+println('VAlue  :: ' + c)
+
+String d = findTestData('login_users').getValue(11, 1)
+
+println('VAlue  :: ' + d)
+
+WebUI.setText(findTestObject('test/Page_PayPlatter  Home  payplatter.i/input_loginBean.userName'), c)
+
+System.out.println(Encryptor.decrypt('passwordmuskilba', '1234567898765432', d))
+
+String actPassword = null
+
+try {
+    actPassword = Encryptor.decrypt('passwordmuskilba', '1234567898765432', d)
+}
+catch (Exception e) {
+    e.printStackTrace()
+} 
+
+WebUI.setText(findTestObject('test/Page_PayPlatter  Home  payplatter.i/input_loginBean.password'), actPassword)
+
+//WebUI.setText(findTestObject('test/Page_PayPlatter  Home  payplatter.i/input_loginBean.password'), 'SAdmin@2018')
+WebUI.click(findTestObject('test/Page_PayPlatter  Home  payplatter.i/input'))
+
+/*try {
+	println("In Mail Send ::::::::::::::::::::::::::::::::::::::::::::::::::")
+	CustomKeywords.'com.email.Email.sendMail'('kumar.ravi7852@gmail.com', 'test', 'www.gmail.com')
+	} catch (Exception e) {
+	// TODO Auto-generated catch block
+	println ("Exception :: " +e.getMessage())
+	e.printStackTrace();
+	}*/
+WebUI.mouseOver(findTestObject('create payment processor/manage'))
 
